6950379cd1ba232cc45064ac1b18147fc79b5636
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-30 09:47:07 +0000

    wrote functions for savings account and current account

--- bank-account.py
+++ bank-account.py
@@ -22,8 +22,45 @@ class Account(ABC):
     def printSummary(self):
         pass
 
-class savingsAccount:
-    pass
+class savingsAccount(Account):
+    def __init__(self, accountNumber, balance, interestRate):
+        super().__init__(accountNumber, balance)
 
-class currentAccount:
-    pass
+        self.interestRate = interestRate
+
+    def calcInterest(self):
+        return self.balance * self.interestRate
+
+    def withdrawMoney(self, amount):
+        if amount <= self.balance:
+            self.balance = self.balance - amount
+        else:
+            print("Insufficient funds")
+        
+    def printSummary(self):
+        print("=== Savings Account Summary ===")
+        print(f"Account number: {self.accountNumber}")
+        print(f"Balance: {self.balance}")
+        print(f"Interest rate: {self.interestRate}")
+        print("===============================")
+
+class currentAccount(Account):
+    def __init__(self, accountNumber, balance, transactionFee):
+        super().__init__(accountNumber, balance)
+
+        self.transactionFee = transactionFee
+
+    def withdrawMoney(self, amount):
+        if amount + self.transactionFee <= self.balance:
+            self.balance = self.balance - (amount + self.transactionFee)
+        else:
+            print("Insufficient funds")
+
+    def printSummary(self):
+        print("=== Current Account Summary ===")
+        print(f"Account number: {self.accountNumber}")
+        print(f"Balance: {self.balance}")
+        print(f"Interest rate: {self.transactionFee}")
+        print("===============================")
+
+    
